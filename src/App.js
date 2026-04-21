0801bbf7a892eba61afc25416c04bfa3335b1e3c
igor <mr4ort@gmail.com>
2026-04-21 14:54:55 +0200

    Refactor: Consolidate Dashboard folder structure and update import paths.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/Dashboard/Dashboard';
 import './styles/App.css';
 
 function App() {
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
-import Header from '../components/Header/Header';
-import Sidebar from '../components/Sidebar/Sidebar';
-import KanbanBoard from '../components/KanbanBoard/KanbanBoard';
-import TaskModal from '../components/TaskModal/TaskModal';
+import Header from '../../components/Header/Header';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import KanbanBoard from '../../components/KanbanBoard/KanbanBoard';
+import TaskModal from '../../components/TaskModal/TaskModal';
 import './Dashboard.css';
-import {KanbanWrapper} from "../components/KanbanWrapper/KanbanWrapper";
+import {KanbanWrapper} from "../../components/KanbanWrapper/KanbanWrapper";
 
 function Dashboard() {
 
